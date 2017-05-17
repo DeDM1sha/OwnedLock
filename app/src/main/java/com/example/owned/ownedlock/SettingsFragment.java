9f76e9132733688a52bfbfcbe42ff75985ee10f5
DeDM1sha <misha739@yandex.ru>
2017-05-17 15:11:18 +0300

    new settings, fragment for time settings

--- app/src/main/java/com/example/owned/ownedlock/SettingsFragment.java
+++ app/src/main/java/com/example/owned/ownedlock/SettingsFragment.java
@@ -1,13 +1,17 @@
 package com.example.owned.ownedlock;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
 
@@ -28,11 +32,18 @@ public class SettingsFragment extends Fragment {
         Switch_12h = (Switch) view.findViewById (R.id.Switch_12h);
         Switch_24h = (Switch) view.findViewById (R.id.Switch_24h);
         Reverse = (Switch) view.findViewById (R.id.Switch_Reverse);
+
+        final Switch hourSwitch = (Switch) view.findViewById(R.id.Switch_Hours);
+        final Switch minuteSwitch = (Switch) view.findViewById(R.id.Switch_Minutes);
+        final Switch secondSwitch = (Switch) view.findViewById(R.id.Switch_Seconds);
+
         Switch_12h.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick (View e) {
                 if (Switch_12h.isChecked()) {
                     settingsResource.setSwitch_12h (true);
+                    Switch_12h.setClickable(false);
+                    Switch_24h.setClickable(true);
                         if (Switch_24h.isChecked())
                             settingsResource.setSwitch_24h (false);
                 }
@@ -43,11 +54,15 @@ public class SettingsFragment extends Fragment {
             public void onClick (View e) {
                 if (Switch_24h.isChecked()) {
                     settingsResource.setSwitch_24h (true);
+                    Switch_24h.setClickable(false);
+                    Switch_12h.setClickable(true);
                     if (Switch_12h.isChecked())
                         settingsResource.setSwitch_12h (false);
                 }
             }
         });
+        Switch_24h.setClickable(false);
+
         Reverse.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View e) {
@@ -57,6 +72,61 @@ public class SettingsFragment extends Fragment {
                     settingsResource.setReverse (false);
             }
         });
+
+
+        hourSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hourSwitch.isChecked()) {
+                    TimeSettingsFragment fragment =
+                            new TimeSettingsFragment(TimeSettingsFragment.HOUR, settingsResource);
+                    FragmentTransaction ft = ((MainActivity)getActivity()).getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content, fragment);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                } else {
+                    // off changed
+                }
+
+            }
+        });
+
+        minuteSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (minuteSwitch.isChecked()) {
+                    TimeSettingsFragment fragment =
+                            new TimeSettingsFragment(TimeSettingsFragment.MINUTE, settingsResource);
+                    FragmentTransaction ft = ((MainActivity)getActivity()).getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content, fragment);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                } else {
+                    // off changed
+                }
+
+            }
+        });
+
+        secondSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (secondSwitch.isChecked()) {
+                    TimeSettingsFragment fragment =
+                            new TimeSettingsFragment(TimeSettingsFragment.SECOND, settingsResource);
+                    FragmentTransaction ft = ((MainActivity)getActivity()).getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content, fragment);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                } else {
+                    // off changed
+                }
+
+            }
+        });
+
+
+
         return view;
     }
     class MyTimerTask extends AsyncTask<Void, Void, Void> {
---
+++ app/src/main/java/com/example/owned/ownedlock/TimeSettingsFragment.java
@@ -0,0 +1,60 @@
+package com.example.owned.ownedlock;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimeSettingsFragment extends Fragment {
+    public static final String HOUR = "hour";
+    public static final String MINUTE = "minute";
+    public static final String SECOND = "second";
+
+    private SettingsResource settingsResource;
+    private int title = -1;
+    private int description = -1;
+
+    public TimeSettingsFragment(String type, SettingsResource settingsResource) {
+        this.settingsResource = settingsResource;
+
+        switch (type) {
+            case HOUR:
+                title = R.string.settings_time_title_hour;
+                description = R.string.settings_time_description_hour;
+                break;
+
+            case MINUTE:
+                title = R.string.settings_time_title_minute;
+                description = R.string.settings_time_description_minute;
+                break;
+
+            case SECOND:
+                title = R.string.settings_time_title_second;
+                description = R.string.settings_time_description_second;
+                break;
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_time_settings, container, false);
+
+        ((TextView) view.findViewById(R.id.Title)).setText(title);
+        ((TextView) view.findViewById(R.id.Description)).setText(description);
+
+
+        return view;
+    }
+
+
+
+}
